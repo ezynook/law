3d2da70fd127a8b0b0b2f8e8242f9d06c1ed78dd
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2023-01-18 20:04:00 +0700

    18-01-2023-20:03:59

--- Auth.php
+++ Auth.php
@@ -25,6 +25,59 @@
         /*font-size: 14px;*/
     }
     </style>
+       <style>
+/* Center the loader */
+#loader {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 1;
+  width: 120px;
+  height: 120px;
+  margin: -76px 0 0 -76px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  -webkit-animation: spin 1s linear infinite;
+  animation: spin 1s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+/* Add animation to "page content" */
+.animate-bottom {
+  position: relative;
+  -webkit-animation-name: animatebottom;
+  -webkit-animation-duration: 2s;
+  animation-name: animatebottom;
+  animation-duration: 2s
+}
+
+@-webkit-keyframes animatebottom {
+  from { bottom:-100px; opacity:0 } 
+  to { bottom:0px; opacity:1 }
+}
+
+@keyframes animatebottom { 
+  from{ bottom:-100px; opacity:0 } 
+  to{ bottom:0; opacity:1 }
+}
+
+#myDiv {
+  display: none;
+}
+.login-page{
+    height: 10vh;
+}
+</style>
 </head>
 <code>
     <!-- PHP Code -->
@@ -79,12 +132,12 @@
 <!-- End PHP Code -->
 </code>
 
-<body class="login-page iframe-mode w3-container w3-center w3-animate-opacity" style="height: 100%;">
-    <div class="login-box">
+<body onload="myFunction()">
+    <div id="loader"></div>
+    <div class="login-box animate-bottom" style="display:none;" id="myDiv">
         <div class="login-logo">
             <a href="#"><b>Law</b> System</a>
         </div>
-
         <div class="card">
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Login as Administrator</p>
@@ -138,7 +191,18 @@
         }
     }
     </script>
+    <script>
+    var myVar;
 
+    function myFunction() {
+        myVar = setTimeout(showPage, 1000);
+    }
+
+    function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+    }
+    </script>
 </body>
 
 </html>
